c3780419421b1ac5aed4d87f34dbafe191f1a38f
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-06-07 23:58:53 +0200

    start spring

---
+++ src/main/java/de/wwi2020sebevsgruppe1/instrumentenverleih/InstrumentenverleihApplication.java
@@ -0,0 +1,13 @@
+package de.wwi2020sebevsgruppe1.instrumentenverleih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstrumentenverleihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstrumentenverleihApplication.class, args);
+	}
+
+}
